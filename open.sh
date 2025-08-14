019109a16e6177f611055f2161eb2fd4afb33968
ClaudioMSJ <50308248+ClaudioMSJ@users.noreply.github.com>
2025-08-14 16:44:22 -0300

    Update open.sh

--- open.sh
+++ open.sh
@@ -45,11 +45,6 @@ uci set https-dns-proxy.dns.listen_port="5053"
 uci delete dhcp.@dnsmasq[0].server
 uci add_list dhcp.@dnsmasq[0].server='127.0.0.1#5053'
 uci set dhcp.@dnsmasq[0].noresolv='1'
-uci set dhcp.@dnsmasq[0].strictorder='1'
-uci set dhcp.@dnsmasq[0].negttl='0'
-uci set dhcp.@dnsmasq[0].dnsforwardmax='300'
-uci set dhcp.@dnsmasq[0].filterwin2k='1'
-uci set dhcp.@dnsmasq[0].logqueries='0'
 uci set dhcp.@dnsmasq[0].dhcpv6='disabled'
 
 # ==== Bloqueio DNS Direto (somente IPv4) ====
