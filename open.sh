da3be58d8833dd8e39ceefe1cc142c7e29fe0b26
ClaudioMSJ <50308248+ClaudioMSJ@users.noreply.github.com>
2025-08-16 10:32:39 -0300

    Update open.sh

--- open.sh
+++ open.sh
@@ -64,8 +64,8 @@ EOF
 chmod +x /root/adblock.sh
 
 # ==== rc.local sem sobrescrever completamente ====
-grep -qxF 'sh /root/adblock.sh' /etc/rc.local || sed -i '/^exit 0/i sleep 30 && sync && sh /root/adblock.sh' /etc/rc.local
-grep -qxF 'echo 3 > /proc/sys/vm/drop_caches' /etc/rc.local || sed -i '/^exit 0/i sleep 30 && echo 3 > /proc/sys/vm/drop_caches' /etc/rc.local
+grep -qxF 'sh /root/adblock.sh' /etc/rc.local || sed -i '/^exit 0/i sleep 30 && sh /root/adblock.sh' /etc/rc.local
+grep -qxF 'echo 3 > /proc/sys/vm/drop_caches' /etc/rc.local || sed -i '/^exit 0/i sleep 60 && sync && echo 3 > /proc/sys/vm/drop_caches' /etc/rc.local
 
 # ==== Cron Jobs ====
 (crontab -l 2>/dev/null; echo '0 6 * * * sync && echo 3 > /proc/sys/vm/drop_caches') | crontab -
