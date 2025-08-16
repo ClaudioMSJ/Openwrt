b175d2195bcfb5e7fb5e3a4d16fc27a5a8d34769
ClaudioMSJ <50308248+ClaudioMSJ@users.noreply.github.com>
2025-08-16 10:11:17 -0300

    Update open_lean.sh

--- open_lean.sh
+++ open_lean.sh
@@ -35,8 +35,6 @@ uci set network.wan.dns='127.0.0.1'
 
 # ==== Dnsmasq Config ====
 uci set dhcp.@dnsmasq[0].noresolv='1'
-uci -q delete dhcp.@dnsmasq[0].server
-uci add_list dhcp.@dnsmasq[0].server='127.0.0.1#5053'
 uci set dhcp.@dnsmasq[0].cachesize='2000'
 uci set dhcp.@dnsmasq[0].min_cache_ttl='120'
 uci set dhcp.@dnsmasq[0].max_cache_ttl='86400'
